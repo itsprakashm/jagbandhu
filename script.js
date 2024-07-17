3735c7ca14d0f61bf5279894e1154270d9dc2870
Prakash <itsmemr.mp@gmail.com>
2024-07-17 11:40:03 +0530

    updated wheel

--- script.js
+++ script.js
@@ -19,6 +19,15 @@ function shuffle(array) {
 }
 
 function spin() {
+  let cookieitem = getCookie("selectedItem");
+  if (cookieitem != "") {
+    swal(
+      "Congratulations",
+      "You Won The " + cookieitem + ".",
+      "success"
+    );
+    return false;
+  }
   // Play the sound
   wheel.play();
   // Inisialisasi variabel
@@ -51,13 +60,13 @@ function spin() {
   // console.log(Hasil[0]);
 
   // Ambil value item yang terpilih
-  if (MagicRoaster.includes(Hasil[0])) SelectedItem = "Magic Roaster";
-  if (Sepeda.includes(Hasil[0])) SelectedItem = "Sepeda Aviator";
-  if (RiceCooker.includes(Hasil[0])) SelectedItem = "Rice Cooker Philips";
-  if (LunchBox.includes(Hasil[0])) SelectedItem = "Lunch Box Lock&Lock";
-  if (Sanken.includes(Hasil[0])) SelectedItem = "Air Cooler Sanken";
-  if (Electrolux.includes(Hasil[0])) SelectedItem = "Electrolux Blender";
-  if (JblSpeaker.includes(Hasil[0])) SelectedItem = "JBL Speaker";
+  if (MagicRoaster.includes(Hasil[0])) SelectedItem = "8% Discount";
+  if (Sepeda.includes(Hasil[0])) SelectedItem = "16% Discount";
+  if (RiceCooker.includes(Hasil[0])) SelectedItem = "5% Discount";
+  if (LunchBox.includes(Hasil[0])) SelectedItem = "Better luck next time";
+  if (Sanken.includes(Hasil[0])) SelectedItem = "14% Discount";
+  if (Electrolux.includes(Hasil[0])) SelectedItem = "Flat 20Rs Off";
+  if (JblSpeaker.includes(Hasil[0])) SelectedItem = "15% Discount";
 
   // Proses
   box.style.setProperty("transition", "all ease 5s");
@@ -75,11 +84,33 @@ function spin() {
       "You Won The " + SelectedItem + ".",
       "success"
     );
+    var now = new Date();
+    now.setTime(now.getTime() + 1 * 3600 * 1000);
+    cookievalue =  SelectedItem + ";"
+
+    document.cookie="selectedItem=" + cookievalue;
+    document.cookie = "expires=" + now.toUTCString() + ";"
   }, 5500);
 
   // Delay and set to normal state
-  setTimeout(function () {
-    box.style.setProperty("transition", "initial");
-    box.style.transform = "rotate(90deg)";
-  }, 6000);
+  // setTimeout(function () {
+  //   box.style.setProperty("transition", "initial");
+  //   box.style.transform = "rotate(90deg)";
+  // }, 6000);
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
 }
